cf1c505fce04d9bff72153e884ed7bee98aa1f39
mjmonge <matthew.monge@marquette.edu>
2021-01-05 16:55:35 -0600

    Updates

---
+++ precache-manifest.42f1a1041963e4b1f79715a9a3d960b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0ae0cc8236a3aa1c248",
+    "url": "/Laptop-Customizer/static/css/main.1c0c94d3.chunk.css"
+  },
+  {
+    "revision": "d0ae0cc8236a3aa1c248",
+    "url": "/Laptop-Customizer/static/js/main.d0ae0cc8.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/Laptop-Customizer/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "1c3ba3f89eb7ec24de5a",
+    "url": "/Laptop-Customizer/static/js/runtime~main.1c3ba3f8.js"
+  },
+  {
+    "revision": "3a058d125d65f7009c1a1b3674ea25db",
+    "url": "/Laptop-Customizer/index.html"
+  }
+];
