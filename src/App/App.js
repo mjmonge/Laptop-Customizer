05107c41007aa83cc0123a0aff7c756402aed1a1
mjmonge <matthew.monge@marquette.edu>
2021-01-05 16:57:24 -0600

    Factored out app

---
+++ src/App/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import './App.css';
+import Cart from '../Cart/Cart'
+import FeaturesList from '../FeaturesList/FeaturesList'
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selected: {
+        'Processor': {
+          name: '17th Generation Intel Core HB (7 Core with donut spare)',
+          cost: 700
+        },
+        "Operating System": {
+          name: 'Ubuntu Linux 16.04',
+          cost: 200
+        },
+        "Video Card":{
+          name: 'Toyota Corolla 1.5v',
+          cost: 1150.98
+        },
+        "Display": {
+          name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+          cost: 1500
+        }
+      }
+    }
+  }
+
+  updateFeature = (feature, newValue) => {
+    const selected = Object.assign({}, this.state.selected);
+    selected[feature] = newValue;
+    this.setState({
+      selected
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>ELF Computing</h1>
+          <h3>Laptops</h3>
+          <h5>Customize your laptop</h5>  
+        </header>      
+        <main>
+          <FeaturesList 
+            features={this.props.features} 
+            selected={this.state.selected}
+            onSelect={this.updateFeature}
+          />
+          <Cart selected={this.state.selected} />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;  
